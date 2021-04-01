787fcd0725e626e9924351542ce4459616df7d5f
philwoz <61164019+philwoz@users.noreply.github.com>
2021-04-01 20:22:48 +0100

    added code to server.js

---
+++ server.js
@@ -0,0 +1,26 @@
+require("./src/db/connection");
+
+const express = require("express");
+const {userRouter} = require("./src/routes/user");
+const { postRouter } = require("./src/routes/posts");
+
+const port = process.env.PORT || 5000;
+// init instance of express
+const app = express();
+
+// middleware
+app.use(express.json());
+
+app.use(userRouter);
+app.use(postRouter);
+
+//routes/endpoints
+app.get("/health", (req, res) => {
+    res.send({ message: "API is working correctly"})
+});
+
+//server
+app.listen(port, () => {
+    console.log(`Server listening on port ${port}`)
+});
+
