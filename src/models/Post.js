71a8c631a547ae633d28cc43b2149a6379c76f8b
philwoz <61164019+philwoz@users.noreply.github.com>
2021-04-01 22:31:43 +0100

    added code to User & Post models

---
+++ src/models/Post.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+const postSchema = new mongoose.Schema(
+    {
+        title: {
+            type: String,
+            required: true,
+        },
+        content: {
+            type: String,
+            required: true,
+        },
+        author: {
+            type: mongoose.Schema.Types.ObjectId,
+            require: true,
+        },
+    },
+    { timestamps: true }
+
+);
+
+const Post = mongoose.model("Post", postSchema);
+
+module.exports = {
+    Post,
+};
